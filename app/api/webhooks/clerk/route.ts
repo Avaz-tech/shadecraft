07d575e3309a467e9828a5ff23250b2261552d92
Ben96 <avazravshan0@gmail.com>
2024-11-08 17:53:21 +0900

    clerk client users type error fix

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -15,11 +15,11 @@ export async function POST(req: Request) {
     throw new Error("Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local");
   }
 
- // Get the headers
- const headerPayload = headers()
- const svix_id = (await headerPayload).get('svix-id')
- const svix_timestamp = (await headerPayload).get('svix-timestamp')
- const svix_signature = (await headerPayload).get('svix-signature')
+  // Get the headers
+  const headerPayload = headers();
+  const svix_id = (await headerPayload).get("svix-id");
+  const svix_timestamp = (await headerPayload).get("svix-timestamp");
+  const svix_signature = (await headerPayload).get("svix-signature");
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
@@ -69,10 +69,11 @@ export async function POST(req: Request) {
     };
     const newUser = await createUser(user);
 
+    const client = typeof clerkClient === "function" ? await clerkClient() : clerkClient;
+
     // Set public metadata
     if (newUser) {
-      //   @ts-expect-error: should updeate metaData
-      await clerkClient.users.updateUserMetadata(id, {
+      await client.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
         },
