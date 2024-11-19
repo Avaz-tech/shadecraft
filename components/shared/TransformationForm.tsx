9b16dfda09e7729953c71b300b35efa006ead36b
Ben96 <avazravshan0@gmail.com>
2024-11-19 16:53:45 +0900

    image transformation: submit handle

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -9,22 +9,27 @@ import { CustomField } from "./CustomField";
 import { Input } from "../ui/input";
 
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { useState, useTransition } from "react";
+import { useEffect,  useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { Button } from "../ui/button";
 import { updateCredits } from "@/lib/actions/user.action";
 import MediaUploader from "./MediaUploader";
 import TranformedImage from "./TranformedImage";
+import { getCldImageUrl } from "next-cloudinary";
+import { addImage, updateImage } from "@/lib/actions/image.actions";
+import { useRouter } from "next/navigation";
 
 export const formSchema = z.object({
   title: z.string(),
   aspectRatio: z.string().optional(),
   color: z.string().optional(),
-  promt: z.string().optional(),
+  prompt: z.string().optional(),
   publicId: z.string(),
 });
 
 const TransformationForm = ({ action, data = null, userId, type, creditBalance, config = null }: TransformationFormProps) => {
+ 
+
   const transformationType = transformationTypes[type];
   const [image, setImage] = useState(data);
   const [newTransformation, setNewTransformation] = useState<Transformations | null>(null);
@@ -32,6 +37,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
   const [isTransforming, setIsTransforming] = useState(false);
   const [transformationConfig, setTransformationConfig] = useState(config);
   const [isPending, startTransition] = useTransition();
+  const router = useRouter();
 
   const initialValues =
     data && action === "Update"
@@ -39,7 +45,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
           title: data?.title,
           aspectRatio: data?.aspectRatio,
           color: data?.color,
-          prompt: data?.promp,
+          prompt: data?.prompt,
           publicId: data?.publicId,
         }
       : defaultValues;
@@ -51,8 +57,75 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
   });
 
   // # Define a submit handler
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsSubmitting(true);
+
+    if (data || image) {
+      const transformationUrl = getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      });
+
+      const imageData = {
+        title: values.title,
+        publicId: image?.publicId,
+        transformationType: type,
+        width: image?.width,
+        height: image?.height,
+        config: transformationConfig,
+        secureURL: image?.secureURL,
+        transformationUrl: transformationUrl,
+        aspectRatio: values.aspectRatio,
+        prompt: values.prompt,
+        color: values.color,
+      };
+
+      if (action === "Add") {
+        try {
+          console.log("Here -1.1 -->>", image);
+
+          const newImage = await addImage({
+            image: imageData,
+            userId,
+            path: "/",
+          });
+          console.log("Here -1.2 -->>", image);
+
+          if (newImage) {
+            form.reset();
+            setImage(data);
+            console.log("Here -1.1 -->>", image);
+
+            router.push(`/transformations/${newImage._id}`);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      if (action === "Update") {
+        try {
+          const updatedImage = await updateImage({
+            image: {
+              ...imageData,
+              _id: data._id,
+            },
+            userId,
+            path: `/transformations/${data._id}`,
+          });
+
+          if (updatedImage) {
+            router.push(`/transformations/${updatedImage._id}`);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+
+    setIsSubmitting(false);
   }
 
   //   # Handle onSelect
@@ -76,12 +149,12 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
         ...prevState,
         [type]: {
           ...prevState?.[type],
-          [fieldName === "promt" ? "promt" : "to"]: value,
+          [fieldName === "prompt" ? "prompt" : "to"]: value,
         },
       }));
+    }, 1000)();
 
-      return onChangeField(value);
-    }, 1000);
+    return onChangeField(value);
   };
 
   //  # Handle transform button -> TODO: Update Creditfee to something else
@@ -96,6 +169,12 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
       await updateCredits(userId, creditFee);
     });
   };
+
+  useEffect(() => {
+    if (image && (type === "restore" || type === "removeBackground")) {
+      setNewTransformation(transformationType.config);
+    }
+  }, [image, transformationType.config, type]);
 
   return (
     <Form {...form}>
@@ -135,7 +214,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
           <div className="prompt-field">
             <CustomField
               control={form.control}
-              name="promt"
+              name="prompt"
               formLabel={type === "remove" ? "Object to remove" : "Object to recolor"}
               className="w-full"
               render={({ field }) => (
